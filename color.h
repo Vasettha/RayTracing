5761b7edd94ecba847c615de3c279000c9f92f74
Vasettha <vasetthafu@gmail.com>
2025-08-10 15:09:41 +0700

    Add project files.

---
+++ color.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color)
+{
+	double r = pixel_color.x();
+	double g = pixel_color.y();
+	double b = pixel_color.z();
+
+	int rbyte = static_cast<int>(255.99 * r);
+	int gbyte = static_cast<int>(255.99 * g);
+	int bbyte = static_cast<int>(255.99 * b);
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
